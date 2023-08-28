efd60a3b2843d09fb561858d3ec346464be5efaf
Luna <swagnemite@gmx.de>
2023-08-28 16:31:13 +0200

    sunroof

--- lua/entities/lvs_wheeldrive_e34/shared.lua
+++ lua/entities/lvs_wheeldrive_e34/shared.lua
@@ -188,41 +188,18 @@ function ENT:InitWeapons()
 	weapon.UseableByAI = false
 	weapon.Attack = function( ent ) end
 	weapon.StartAttack = function( ent )
-		if not IsValid( ent.HornSND ) then return end
+		if not IsValid( ent.HornSND ) then
+			self.HornSND = self:AddSoundEmitter( Vector(45,0,20), "lvs/horn1.wav", "lvs/horn1.wav" )
+			self.HornSND:SetSoundLevel( 75 )
+			self.HornSND:SetDoppler( true )
+		end
+
 		ent.HornSND:Play()
 	end
 	weapon.FinishAttack = function( ent )
 		if not IsValid( ent.HornSND ) then return end
 		ent.HornSND:Stop()
 	end
-	self:AddWeapon( weapon )
-
-	local weapon = {}
-	weapon.Icon = Material("lvs/weapons/horn.png")
-	weapon.Ammo = -1
-	weapon.Delay = 0.5
-	weapon.HeatRateUp = 0
-	weapon.HeatRateDown = 0
-	weapon.UseableByAI = false
-	weapon.Attack = function( ent ) end
-	weapon.StartAttack = function( ent )
-		if self._sunRoofReverse then
-			ent:SetSunRoof( math.max( ent:GetSunRoof() - 1, -1 ) )
-
-			if ent:GetSunRoof() <= -1 then
-				self._sunRoofReverse = nil
-			end
-		else
-			ent:SetSunRoof( math.min( ent:GetSunRoof() + 1, 1 ) )
-	
-			if ent:GetSunRoof() >= 1 then
-				self._sunRoofReverse = true
-			end
-		end
-
-		ent:EmitSound("buttons/lever8.wav",75,195,0.5)
-	end
-	weapon.FinishAttack = function( ent ) end
 	self:AddWeapon( weapon )
 end
 

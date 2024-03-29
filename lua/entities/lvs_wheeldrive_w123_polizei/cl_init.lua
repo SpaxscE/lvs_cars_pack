2384bb66969267ab02d966b4473fd388d51bde7c
Luna <swagnemite@gmx.de>
2024-03-29 15:51:24 +0100

    fix no rotation on HOLD activation

--- lua/entities/lvs_wheeldrive_w123_polizei/cl_init.lua
+++ lua/entities/lvs_wheeldrive_w123_polizei/cl_init.lua
@@ -31,7 +31,7 @@ function ENT:UpdatePoseParameters( steer, speed_kmh, engine_rpm, throttle, brake
 
 	self:SetPoseParameter( "gear",  self:QuickLerp( "gear", (GearIDtoPose[ gear ] or 1) ) )
 
-	if self:GetSirenMode() <= 0 then return end
+	if self:GetSirenMode() < 0 then return end
 
 	self:SetPoseParameter( "siren_spin", CurTime() * 1400 + self:EntIndex() * 1337 )
 

37fb87d182873599854028a79532ae2e8fc53ad8
Diggergithub <diggerddat@yandex.ru>
2024-04-02 00:21:52 +0500

    trans am

---
+++ lua/entities/lvs_wheeldrive_pontiac_transam3/init.lua
@@ -0,0 +1,103 @@
+AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "cl_init.lua" )
+include("shared.lua")
+
+function ENT:OnSpawn( PObj )
+	local DriverSeat = self:AddDriverSeat( Vector(-15,14.5,-8), Angle(0,-90,8) )
+	local PassengerSeat = self:AddPassengerSeat( Vector(4,-14,9), Angle(0,-90,28) )
+
+	self:AddPassengerSeat( Vector(-24,-15,5), Angle(0,-90,15) )
+	self:AddPassengerSeat( Vector(-24,15,5), Angle(0,-90,15) )
+
+	self:AddEngine( Vector(64.5,0,20.24) )
+	--self:AddSuperCharger()
+
+	local DoorHandler = self:AddDoorHandler( "left_door", Vector(0,33,20), Angle(0,0,0), Vector(-15,-6,-16), Vector(38,6,8), Vector(-15,-20,-16), Vector(38,42,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( DriverSeat )
+
+	local DoorHandler = self:AddDoorHandler( "right_door", Vector(0,-33,20), Angle(0,0,0), Vector(-15,-6,-16), Vector(38,6,8), Vector(-15,-42,-16), Vector(38,20,8) )
+	DoorHandler:SetSoundOpen( "lvs/vehicles/generic/car_door_open.wav" )
+	DoorHandler:SetSoundClose( "lvs/vehicles/generic/car_door_close.wav" )
+	DoorHandler:LinkToSeat( PassengerSeat )
+
+	local LightsHandler = self:GetLightsHandler()
+	if IsValid( LightsHandler ) then
+		local DoorHandler = self:AddDoorHandler( "lights", Vector(72,0,20), Angle(0,0,0), Vector(-5,-25,-5), Vector(5,25,5) )
+		DoorHandler:SetRate( 4 )
+		DoorHandler:SetRateExponent( 1 )
+
+		LightsHandler:SetDoorHandler( DoorHandler )
+	end
+
+	self:AddFuelTank( Vector(-86,0,10), Angle(0,0,0), 600, LVS.FUELTYPE_PETROL )
+
+	local WheelModel = "models/diggercars/pontiac_transam3/wheel.mdl"
+
+	local FrontAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_FRONT,
+			SteerAngle = 30,
+			TorqueFactor = 0,
+			BrakeFactor = 1,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(50.6,30,6),
+				mdl = WheelModel,
+				mdl_ang = Angle(0,180,0),
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(50.6,-30,6),
+				mdl = WheelModel,
+				mdl_ang = Angle(0,0,0),
+			} ),
+		},
+		Suspension = {
+			Height = 8,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+
+	local RearAxle = self:DefineAxle( {
+		Axle = {
+			ForwardAngle = Angle(0,0,0),
+			SteerType = LVS.WHEEL_STEER_NONE,
+			TorqueFactor = 1,
+			BrakeFactor = 1,
+			UseHandbrake = true,
+		},
+		Wheels = {
+			self:AddWheel( {
+				pos = Vector(-50.6,30,6),
+				mdl = WheelModel,
+				mdl_ang = Angle(0,180,0),
+			} ),
+
+			self:AddWheel( {
+				pos = Vector(-50.6,-30,6),
+				mdl = WheelModel,
+				mdl_ang = Angle(0,0,0),
+			} ),
+		},
+		Suspension = {
+			Height = 9,
+			MaxTravel = 7,
+			ControlArmLength = 25,
+			SpringConstant = 20000,
+			SpringDamping = 2000,
+			SpringRelativeDamping = 2000,
+		},
+	} )
+
+end
+
+
+
---
+++ lua/entities/lvs_wheeldrive_pontiac_transam3/shared.lua
@@ -0,0 +1,153 @@
+
+ENT.Base = "lvs_base_wheeldrive"
+
+ENT.PrintName = "Pontiac Trans Am"
+ENT.Author = "Digger"
+ENT.Information = "Luna's Vehicle Script"
+ENT.Category = "[LVS] - Cars - Pack"
+
+ENT.VehicleCategory = "Cars"
+ENT.VehicleSubCategory = "Civilian"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+ENT.MDL = "models/diggercars/pontiac_transam3/transam.mdl"
+
+ENT.MaxVelocity = 2200
+
+ENT.EngineTorque = 120
+ENT.EngineIdleRPM = 750
+ENT.EngineMaxRPM = 5000
+
+ENT.TransGears = 4
+ENT.TransGearsReverse = 1
+
+ENT.SuperChargerVisible = false
+
+
+ENT.HornSound = "lvs/horn2.wav"
+ENT.HornPos = Vector(40,0,35)
+
+ENT.EngineSounds = {
+	{
+		sound = "lvs/vehicles/dodge_charger/eng_idle_loop.wav",
+		Volume = 1,
+		Pitch = 85,
+		PitchMul = 25,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_IDLE_ONLY,
+	},
+	{
+		sound = "lvs/vehicles/dodge_charger/eng_loop.wav",
+		Volume = 1,
+		Pitch = 60,
+		PitchMul = 110,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_REV_UP,
+		UseDoppler = true,
+	},
+	{
+		sound = "lvs/vehicles/dodge_charger/eng_revdown_loop.wav",
+		Volume = 1,
+		Pitch = 60,
+		PitchMul = 110,
+		SoundLevel = 75,
+		SoundType = LVS.SOUNDTYPE_REV_DOWN,
+		UseDoppler = true,
+	},
+}
+
+ENT.ExhaustPositions = {
+	{
+		pos = Vector(-100.04,14.72,4.84),
+		ang = Angle(0,180,0),
+	},
+	{
+		pos = Vector(-100.04,-14.72,4.84),
+		ang = Angle(0,180,0),
+	}
+}
+
+ENT.Lights = {
+
+	{
+		Trigger = "main",
+		ProjectedTextures = {
+			{ pos = Vector(84.53,24.61,18.32), ang = Angle(7,0,0), colorB = 200, colorA = 150, shadows = true },
+			{ pos = Vector(84.53,-24.61,18.32), ang = Angle(7,0,0), colorB = 200, colorA = 150, shadows = true },
+		},
+	},
+	{
+		Trigger = "high",
+		ProjectedTextures = {
+			{ pos = Vector(84.53,24.61,18.32), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
+			{ pos = Vector(84.53,-24.61,18.32), ang = Angle(0,0,0), colorB = 200, colorA = 150, shadows = true },
+		},
+	},
+	{
+	Trigger = "main+high",
+		SubMaterialID = 20,
+		Sprites = {
+			{ pos = Vector(84.53,24.61,18.32), colorB = 200, colorA = 150 },
+			{ pos = Vector(84.53,-24.61,18.32), colorB = 200, colorA = 150 },
+		},
+	},
+
+	{
+		Trigger = "main+brake",
+		SubMaterialID = 17,
+		Sprites = {
+			{ pos = Vector(-89.47,21.99,20.61), colorG = 0, colorB = 0, colorA = 50 },
+			{ pos = Vector(-90,15.9,20.65), colorG = 0, colorB = 0, colorA = 50 },
+
+			{ pos = Vector(-89.47,-21.99,20.61), colorG = 0, colorB = 0, colorA = 50 },
+			{ pos = Vector(-90,-15.9,20.65), colorG = 0, colorB = 0, colorA = 50 },
+		}
+	},
+	{
+		Trigger = "main",
+		SubMaterialID = 0,
+	},
+	{
+		Trigger = "main+brake+turnleft",
+		SubMaterialID = 19,
+		Sprites = {
+			{ pos = Vector(-88.88,27.8,20.62), width = 50, height = 50, colorG = 0, colorB = 0, colorA = 50, mat = "sprites/lvs/glow_smooth" },
+		},
+	},
+	{
+		Trigger = "main+brake+turnright",
+		SubMaterialID = 18,
+		Sprites = {
+			{ pos = Vector(-88.88,-27.8,20.62), width = 50, height = 50, colorG = 0, colorB = 0, colorA = 50, mat = "sprites/lvs/glow_smooth" },
+		},
+	},
+	{
+		Trigger = "turnright",
+		SubMaterialID = 6,
+		Sprites = {
+			{ width = 40, height = 40, pos = Vector(86.17,-13.98,15.96), colorG = 100, colorB = 0, colorA = 150 },
+			{ width = 40, height = 20, pos = Vector(79.2,-32.85,11.49), colorG = 100, colorB = 0, colorA = 150 },
+			{ width = 40, height = 20, pos = Vector(-80.69,-33.68,12.69), colorG = 0, colorB = 0, colorA = 150 },
+
+		},
+	},
+	{
+		Trigger = "turnleft",
+		SubMaterialID = 5,
+		Sprites = {
+			{ width = 40, height = 40, pos = Vector(86.17,13.98,15.96), colorG = 100, colorB = 0, colorA = 150 },
+			{ width = 40, height = 20, pos = Vector(79.2,32.85,11.49), colorG = 100, colorB = 0, colorA = 150 },
+			{ width = 40, height = 20, pos = Vector(-80.69,33.68,12.69), colorG = 0, colorB = 0, colorA = 150 },
+		},
+	},
+	{
+		Trigger = "reverse",
+		SubMaterialID = 16,
+		Sprites = {
+			{ pos = Vector(-90.06,-10.41,20.67), height = 25, width = 25, colorA = 150 },
+			{ pos = Vector(-90.06,10.41,20.67), height = 25, width = 25, colorA = 150 },
+		}
+	},
+}

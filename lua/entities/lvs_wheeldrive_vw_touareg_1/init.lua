2c478d6d262f34701883b456a3a308f2a1107b64
SpaxscE <stephanklings@freenet.de>
2026-05-04 23:09:01 +0200

    Update init.lua

--- lua/entities/lvs_wheeldrive_vw_touareg_1/init.lua
+++ lua/entities/lvs_wheeldrive_vw_touareg_1/init.lua
@@ -38,7 +38,7 @@ function ENT:OnSpawn( PObj )
 	DoorHandler:LinkToSeat( PassengerSeat2 )
 
 
-	local FuelTank = self:AddFuelTank( Vector(-60,0,10), Angle(0,0,0), 4000, LVS.FUELTYPE_PETROL )
+	local FuelTank = self:AddFuelTank( Vector(-60,0,10), Angle(0,0,0), 4000, LVS.FUELTYPE_DIESEL )
 
 	local WheelModel = "models/diggercars/vw_touareg/wheel.mdl"
 
